36f0815cb89ae8f14c178beed68e5c298188ba81
NathanielAbalos <143874233+NathanielAbalos@users.noreply.github.com>
2023-09-02 17:19:45 +0800

    Add files via upload

---
+++ ctuSite.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector(".hamburger");
+const navMobile = document.querySelector(".navBar");
+const dateDisplay = document.querySelector(".date");
+const dropDownButton = document.querySelectorAll(".dropDownButton");
+let date = new Date();
+
+hamburger.addEventListener("click",(event)=>{
+    hamburger.classList.toggle("active");
+    navMobile.classList.toggle("active");
+});
+
+for(let i = 0; i<dropDownButton.length; i++){
+dropDownButton[i].addEventListener("click",(event) =>{
+    if(dropDownButton[i].nextElementSibling.style.display==="none"){
+        dropDownButton[i].nextElementSibling.style.display="block";
+    }
+    else{
+        dropDownButton[i].nextElementSibling.style.display="none";
+    }
+});
+};
+
+
+window.onload=function(){
+    let hours = date.getHours();
+    let min = date.getMinutes();
+    let dayOfMonth = date.getDate();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    
+    if(dayOfMonth<10&&month<10){
+        dateDisplay.textContent=`As of 0${month+1}/0${dayOfMonth}/${year}, here are our latest updates`;
+    }
+    else{
+        dateDisplay.textContent=`As of ${month+1}/${dayOfMonth}/${year}, here are our latest updates`;
+    }
+}
